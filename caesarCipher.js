613435bd7af9e3a945743863b492a9d5dc271c97
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2022-06-25 19:58:24 +0430

    'str' cannot contain accented letters!
    Added "|| /[ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž]/.test(str)" to the code to exclude accents.

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,5 +1,5 @@
 const caesarCipher = (str, n) =>
-    n < 0 || n > 26 || !Number.isInteger(n) ? new Error("'n' must be an integer such that 0 ≤ n ≤ 26.") : str.toLowerCase().replace(
+    n < 0 || n > 26 || !Number.isInteger(n) || /[ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž]/.test(str) ? new Error("'n' must be an integer such that 0 ≤ n ≤ 26, and 'str' cannot contain accented letters.") : str.toLowerCase().replace(
         /[a-z]/gi,
         (c) =>
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"[
