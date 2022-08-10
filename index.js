2a25fdb8b3a4a1b1dc255e88c036e4709b202df5
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2022-08-10 18:13:04 +0430

    n ---> parseInt(n)

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const caesarCipher = (str, n) =>
-    n < 0 || n > 26 || !Number.isInteger(n) || /[ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž]/.test(str) ? new Error("'n' must be an integer such that 0 ≤ n ≤ 26, and 'str' cannot contain accented letters.") : str.toLowerCase().replace(
+parseInt(n) < 0 || parseInt(n) > 26 || !Number.isInteger(parseInt(n)) || /[ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž]/.test(str) ? new Error("'n' must be an integer such that 0 ≤ n ≤ 26, and 'str' cannot contain accented letters.") : str.toLowerCase().replace(
         /[a-z]/gi,
         (c) =>
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"[
-            "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz".indexOf(c) + n]
+            "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz".indexOf(c) + parseInt(n)]
     );
 
-module.exports = { caesarCipher };
+export default caesarCipher;
